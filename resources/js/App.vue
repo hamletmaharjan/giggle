213f01eb5e3129b7996b4e871b98bdd40c6eaa38
hamlet <imhamlet.hams@gmail.com>
2020-03-26 23:22:28 +0545

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>
+    Laravel
+    </h1>
+</template>
+
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+import Vue from './vue';
+import App from './App';
+
+
+new Vue({
+    render: function(createElement){
+        return createElement(App);
+    }
+}).$mount('#app');
