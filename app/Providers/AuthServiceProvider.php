3459329507b04be2f08c53842019e52f8e11a3c9
hamlet <imhamlet.hams@gmail.com>
2020-03-31 22:29:17 +0545

    basic api login with vue

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-        //Passport::tokensExpireIn(now()->addDays(15));
+        Passport::tokensExpireIn(now()->addDays(15));
 
     }
 }
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,67 @@
 <template>
-    <Clients></Clients>
+    <div>
+        <Login v-on:loginData="login"></Login>
+        {{token}}
+    </div>
+    
 </template>
 
 
 <script>
-import Clients from './components/passport/Clients.vue';
+import axios from 'axios';
+import Login from './components/Login.vue';
 export default {
     name: 'App',
+    data() {
+        return {
+            token:''
+        }
+    },
     components: {
-        Clients
+        Login
+    },
+    methods: {
+        login: function(loginData){
+            // axios.post('https://www.googleapis.com/youtube/v3/search',{
+            //     headers:{
+            //         'Content-Type': 'application/json',
+            //         'X-Requested-With': 'XMLHttpRequest'
+            //     },
+            //     body: {
+            //         email: loginData.email,
+            //         password: loginData.password
+            //     }
+            // }).then(response => {
+            //     console.log(response);
+            // });
+
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("X-Requested-With", "XMLHttpRequest");
+
+            var raw = JSON.stringify({"email":loginData.email,"password":loginData.password});
+
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+
+            fetch("http://localhost:8000/api/auth/login", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                
+                var data = JSON.parse(result);
+                console.log(data);
+                this.token = data.access_token;
+            })
+            .catch(error => console.log('error', error));
+
+            console.log(this.token);
+        }
     }
 }
 </script>
+
 
